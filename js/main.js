677272076988edf0368c7560ac2ca02b46d61284
Shuyang <shuyang0129@gmail.com>
2017-07-04 17:49:18 +0800

    search function

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$.noConflict();
+jQuery(document).ready(function($){
+
+	var itemNum = $(".item").length;
+	var search = "";
+	var title = "";
+	var description = "";
+
+    $("#search").on("keyup", function(){
+
+    	var count = 0;
+
+        if (search != $("#search").val()) {
+        	search = $("#search").val();
+        	console.log(search);
+
+        	for (var i = 0; i < itemNum; i++) {
+        		title = $(".item").eq(i).find(".title").text();
+        		description = $(".item").eq(i).find(".description").text();
+        		
+        		if (title.search(search) > -1 || description.search(search) > -1 ) {
+        			
+        			if ( $(".item").eq(i).css("display") === "none" ) {
+        				$(".item").eq(i).removeClass("hide").fadeIn("slow");
+        			}
+
+        			count ++;
+
+        		} else {
+					
+        			$(".item").eq(i).addClass("hide").fadeOut("slow");
+
+        		}
+        	}
+        }
+
+        if ($(".item:not('.hide')").length <= 0) {
+        		$("#no-result").removeClass("hide");
+        	} else {
+        		$("#no-result").addClass("hide");
+        	}
+
+    });
+});
