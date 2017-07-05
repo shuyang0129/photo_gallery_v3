3d040474cd6c4c2a47f15eb36c28aa884cace5eb
Shuyang <shuyang0129@gmail.com>
2017-07-05 15:02:35 +0800

    Create javascript object

---
+++ js/photos-info.js
@@ -0,0 +1,78 @@
+var photos = [
+	
+	{
+		title: "Hay Bales",
+		description: "I love hay bales. Took this snap on a drive through the countryside past some straw fields.",
+		hide: false
+	},
+
+	{
+		title: "Lake",
+		description: "The lake was so calm today. We had a great view of the snow on the mountains from here.",
+		hide: false
+	},
+
+	{
+		title: "Canyon",
+		description: "I hiked to the top of the mountain and got this picture of the canyon and trees below.",
+		hide: false
+	},
+
+	{
+		title: "Iceberg",
+		description: "It was amazing to see an iceberg up close, it was so cold but didn’t snow today.",
+		hide: false
+	},
+
+	{
+		title: "Desert",
+		description: "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.",
+		hide: false
+	},
+
+	{
+		title: "Fall",
+		description: "Fall is coming, I love when the leaves on the trees start to change color.",
+		hide: false
+	},
+
+	{
+		title: "Plantation",
+		description: "I drove past this plantation yesterday, everything is so green!",
+		hide: false
+	},
+
+	{
+		title: "Dunes",
+		description: "My summer vacation to the Oregon Coast. I love the sandy dunes!",
+		hide: false
+	},
+
+	{
+		title: "Countryside Lane",
+		description: "We enjoyed a quiet stroll down this countryside lane.",
+		hide: false
+	},
+
+	{
+		title: "Sunset",
+		description: "Sunset at the coast! The sky turned a lovely shade of orange.",
+		hide: false
+	},
+
+	{
+		title: "Cave",
+		description: "I did a tour of a cave today and the view of the landscape below was breathtaking.",
+		hide: false
+	},
+
+	{
+		title: "Bluebells",
+		description: "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.",
+		hide: false
+	}
+];
+
+
+
+
